e5163949dee74f7ac5017688ffac53b37f003016
Satyajit Ghana <satyajitghana7@gmail.com>
2016-08-19 20:22:48 +0530

    Initial Commit

---
+++ FileHandling2(classes)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FileHandling2(classes)
+//
+//  Created by Satyajit Ghana on 19/08/16.
+//  Copyright © 2016 Satyajit Ghana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
